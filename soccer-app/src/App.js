5b5984ccff94f8371f7889107e3dee52033e0a97
dragon96nfs <dragon96nfs@gmail.com>
2019-07-28 00:58:10 +0430

    Player Part done!

--- soccer-app/src/App.js
+++ soccer-app/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import Button from '@material-ui/core/Button';
+import { Button, AppBar, Toolbar, Typography} from '@material-ui/core';
 import './App.css';
+
+import Player from './imports/ui/Player.js';
+import TeamList from './imports/ui/Team-list.js';
+import TeamStats from './imports/ui/Team-stats.js';
+
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Button variant="contained" color="primary">Okay!</Button>
+        <div className="container">
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Soccer Application
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className="row">
+          <div className="col s12 m7"><Player /></div>
+          <div className="col s12 m5"><TeamStats /></div>
+          <div className="col s12 m5"><TeamList /></div>
+        </div>
+      </div>
       </MuiThemeProvider>
     );
   }
--- soccer-app/src/imports/ui/Player.js
+++ soccer-app/src/imports/ui/Player.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, CardActionArea } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-class Player extends Component {
-  render() {
-    return (
-      <h1>Player</h1>
+
+export default class Player extends Component {
+  render(){
+    return(
+      <Card>
+       <CardActionArea>
+         <CardMedia
+           component="img"
+           alt="Nabil"
+           height="fit"
+           image="nabil.jpg"
+           title="Nabil"
+         />
+         <CardContent>
+           <Typography gutterBottom variant="h5" component="h2">
+             Nabil
+           </Typography>
+           <Typography variant="body2" color="textSecondary" component="p">
+             I'm just a simple player. not a big deal!
+           </Typography>
+         </CardContent>
+       </CardActionArea>
+       <CardActions>
+         <Button size="small" color="primary">
+           Share
+         </Button>
+         <Button size="small" color="primary">
+           Learn More
+         </Button>
+       </CardActions>
+     </Card>
     );
   }
-}
 
-export default Player;
+}
